4cfd174f086098dfad373b36f438e62689008349
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-13 15:37:26 +0530

    create server and database connection

---
+++ CRUD/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/crud")
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
