4581faf1b983ef45656764ed5af97d04b046e7f4
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-13 14:31:51 +0530

    User page

---
+++ CRUD/client/src/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateUser() {
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      
+    </div>
+  )
+}
+
+export default CreateUser
