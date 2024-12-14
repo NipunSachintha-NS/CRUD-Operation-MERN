66d29e796c8f75c06290703eae8992541e76414a
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-14 09:11:27 +0530

    Create  user banckend

--- CRUD/client/src/Users.jsx
+++ CRUD/client/src/Users.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 function Users() {
-   const [users, setUsers] = useState([{
-      Name:"Nipun", Email:"nipun@gamil.com", Age: 24
-   }])
+   const [users, setUsers] = useState([])
+
+   useEffect(()=> {
+    axios.get('http://localhost:3001')
+    .then(result => setUsers(result.data))
+    .catch(err => console.log(err))
+   }, [])
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
         <div className='w-50 bg-white rounded p-3'>
@@ -24,12 +30,12 @@ function Users() {
                     {
                         users.map((user) => {
                            return <tr>
-                                <td>{user.Name}</td>
-                                <td>{user.Email}</td>
-                                <td>{user.Age}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
                                 <td>
                                 <Link to="/Update" className='btn btn-success'>Update</Link>
-                                    <button>Delete</button></td>
+                                    <button className='btn btn-danger'>Delete</button></td>
                             </tr>
                         })
                     }
